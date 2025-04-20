d700a3e524891dffde1a9c61ddcda61e548be730
gw-rodrigues <rodrigues.gw@gmail.com>
2025-04-20 16:24:58 +0200

    feat footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="h-20 flex items-center justify-center border-t">
       <p>© 2025 QuickCart. Todos os direitos reservados.</p>
     </footer>
   )
