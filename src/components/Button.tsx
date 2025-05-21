42e8f6c96d68e2ff56d4674e055a48298051d53e
Gleydson W. Rodrigues <rodrigues.gw@gmail.com>
2025-05-21 18:09:06 +0200

    feat add twmerge

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,15 @@
+import { twMerge } from 'tailwind-merge'
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
 }
 
-export function Button({ children, ...rest }: IButtonProps) {
+export function Button({ children, className, ...rest }: IButtonProps) {
   return (
     <button
-      className="border border-gray-600 rounded w-full xl:w-30 p-4 bg-gray-800 text-gray-50 font-bold hover:bg-orange-400 hover:text-gray-800"
+      className={twMerge(
+        `border border-gray-600 rounded w-full xl:w-30 p-4 bg-gray-800 text-gray-50 font-bold hover:bg-orange-400 hover:text-gray-800 cursor-pointer`,
+        className,
+      )}
       {...rest}
     >
       {children}
