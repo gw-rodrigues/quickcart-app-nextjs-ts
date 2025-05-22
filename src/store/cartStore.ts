ef3d10230df81e4d4e068b74b5fe8241b3a1e5ad
Gleydson W. Rodrigues <rodrigues.gw@gmail.com>
2025-05-22 23:21:15 +0200

    feat cart add, plus and menus

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -1,15 +1,63 @@
 import { create } from 'zustand'
 import { TProduct } from '@/types/Product'
 
+export interface ICartProducts extends TProduct {
+  quantity: number
+}
+
 type TCartState = {
-  items: TProduct[]
+  items: ICartProducts[]
   addItem: (product: TProduct) => void
   removeItem: (id: number) => void
+  plusQuantity: (id: number) => void
+  menusQuantity: (id: number) => void
 }
 
 export const useCart = create<TCartState>((set) => ({
   items: [],
-  addItem: (product) => set((state) => ({ items: [...state.items, product] })),
+  addItem: (product) => {
+    set((state) => {
+      let itemExists = false
+
+      const updatedItems = state.items.reduce((acc, p) => {
+        if (p.id === product.id) {
+          acc.push({ ...p, quantity: p.quantity + 1 })
+          itemExists = true
+        } else {
+          acc.push(p)
+        }
+        return acc
+      }, [] as ICartProducts[])
+
+      if (itemExists) {
+        return {
+          ...state,
+          items: updatedItems,
+        }
+      } else {
+        return {
+          ...state,
+          items: [...state.items, { ...product, quantity: 1 }],
+        }
+      }
+    })
+  },
   removeItem: (id) =>
     set((state) => ({ items: state.items.filter((p) => p.id !== id) })),
+
+  plusQuantity: (id) => {
+    set((state) => ({
+      items: state.items.map((p) =>
+        p.id === id ? { ...p, quantity: p.quantity + 1 } : p,
+      ),
+    }))
+  },
+
+  menusQuantity: (id) => {
+    set((state) => ({
+      items: state.items.map((p) =>
+        p.id === id ? { ...p, quantity: p.quantity - 1 } : p,
+      ),
+    }))
+  },
 }))
