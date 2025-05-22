e4be9d51c531a28fe6a362f36b7cacf590df8f18
Gleydson W. Rodrigues <rodrigues.gw@gmail.com>
2025-05-22 23:20:21 +0200

    fix menu not closing

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -2,14 +2,16 @@ import Link from 'next/link'
 
 interface INavLinksProps extends React.PropsWithChildren {
   href: string
+  toggleMenu: () => void
 }
 
-export function NavLink({ href, children }: INavLinksProps) {
+export function NavLink({ href, toggleMenu, children }: INavLinksProps) {
   return (
     <li className="flex">
       <Link
         className="p-4 md:p-0 flex-1 active:font-bold hover:bg-orange-500 md:hover:bg-gray-50 hover:text-gray-50 md:hover:text-orange-500 transition duration-250 ease-in-out"
         href={href}
+        onClick={toggleMenu}
       >
         {children}
       </Link>
